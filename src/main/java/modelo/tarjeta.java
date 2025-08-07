145f19635af1f880f019715732da0b8c3c2e528f
kannoncoding <codingadaptations@gmail.com>
2025-08-06 20:24:59 -0600

    main object class built

--- src/main/java/modelo/tarjeta.java
+++ src/main/java/modelo/tarjeta.java
@@ -1,13 +1,45 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
 package modelo;
 
-/**
- *
- * @author Pegasus Onr
- */
 public class tarjeta {
-    
+    private int id;
+    private String descripcion;
+    private String categoria;
+
+    // Constructor
+    public tarjeta(int id, String descripcion, String categoria) {
+        this.id = id;
+        this.descripcion = descripcion;
+        this.categoria = categoria;
+    }
+
+    // Getters y Setters
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getDescripcion() {
+        return descripcion;
+    }
+
+    public void setDescripcion(String descripcion) {
+        this.descripcion = descripcion;
+    }
+
+    public String getCategoria() {
+        return categoria;
+    }
+
+    public void setCategoria(String categoria) {
+        this.categoria = categoria;
+    }
+
+    // ToString para mostrar información fácilmente
+    @Override
+    public String toString() {
+        return "ID: " + id + ", Descripción: " + descripcion + ", Categoría: " + categoria;
+    }
 }
